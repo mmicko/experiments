20df9b2f5a33b5ee1c7fd8ba23b526781bb093ec
Miodrag Milanovic <mmicko@gmail.com>
2016-06-19 17:38:29 +0200

    Added initial code

---
+++ scripts/genie.lua
@@ -0,0 +1,258 @@
+--
+-- Copyright 2010-2016 Branimir Karadzic. All rights reserved.
+-- License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
+--
+
+newoption {
+	trigger = "with-sdl",
+	description = "Enable SDL entry.",
+}
+
+newoption {
+	trigger = "with-glfw",
+	description = "Enable GLFW entry.",
+}
+
+newoption {
+	trigger = "with-profiler",
+	description = "Enable build with intrusive profiler.",
+}
+
+
+solution "retroCommander"
+	configurations {
+		"Debug",
+		"Release",
+	}
+
+	if _ACTION == "xcode4" then
+		platforms {
+			"Universal",
+		}
+	else
+		platforms {
+			"x32",
+			"x64",
+--			"Xbox360",
+			"Native", -- for targets where bitness is not specified
+		}
+	end
+
+	language "C++"
+	startproject "retroCommander"
+
+MODULE_DIR = path.getabsolute("../")
+SRC_DIR = path.getabsolute("../src")
+BGFX_DIR   = path.getabsolute("../3rdparty/bgfx")
+BX_DIR     = path.getabsolute("../3rdparty/bx")
+
+local BGFX_BUILD_DIR = path.join("../", "build")
+local BGFX_THIRD_PARTY_DIR = path.join(BGFX_DIR, "3rdparty")
+if not BX_DIR then
+	BX_DIR = path.getabsolute(path.join(BGFX_DIR, "../bx"))
+end
+
+if not os.isdir(BX_DIR) then
+	print("bx not found at " .. BX_DIR)
+	print("For more info see: https://bkaradzic.github.io/bgfx/build.html")
+	os.exit()
+end
+
+defines {
+	"BX_CONFIG_ENABLE_MSVC_LEVEL4_WARNINGS=1"
+}
+
+dofile (path.join(BX_DIR, "scripts/toolchain.lua"))
+if not toolchain(BGFX_BUILD_DIR, BGFX_THIRD_PARTY_DIR) then
+	return -- no action specified
+end
+
+function copyLib()
+end
+
+if _OPTIONS["with-sdl"] then
+	if os.is("windows") then
+		if not os.getenv("SDL2_DIR") then
+			print("Set SDL2_DIR enviroment variable.")
+		end
+	end
+end
+
+if _OPTIONS["with-profiler"] then
+	defines {
+		"ENTRY_CONFIG_PROFILER=1",
+		"BGFX_CONFIG_PROFILER_REMOTERY=1",
+        "_WINSOCKAPI_"
+	}
+end
+
+function retroCommander()
+
+	project ("retroCommander")
+		uuid (os.uuid("retroCommander"))
+		kind "WindowedApp"
+
+	configuration {}
+
+	includedirs {
+		path.join(BX_DIR,   "include"),
+		path.join(BGFX_DIR, "include"),
+		path.join(BGFX_DIR, "3rdparty"),
+		path.join(BGFX_DIR, "examples/common"),
+		path.join(SRC_DIR,  ""),
+	}
+
+	files {
+		path.join(SRC_DIR, "main.cpp"),
+	}
+
+	links {
+		"bgfx",
+		"example-common",
+	}
+
+	if _OPTIONS["with-sdl"] then
+		defines { "ENTRY_CONFIG_USE_SDL=1" }
+		links   { "SDL2" }
+
+		configuration { "osx" }
+			libdirs { "$(SDL2_DIR)/lib" }
+
+		configuration {}
+	end
+
+	if _OPTIONS["with-glfw"] then
+		defines { "ENTRY_CONFIG_USE_GLFW=1" }
+		links   { "glfw3" }
+
+		configuration { "linux or freebsd" }
+			links {
+				"Xrandr",
+				"Xinerama",
+				"Xi",
+				"Xxf86vm",
+				"Xcursor",
+			}
+
+		configuration { "osx" }
+			linkoptions {
+				"-framework CoreVideo",
+				"-framework IOKit",
+			}
+
+		configuration {}
+	end
+
+	configuration { "vs*", "x32 or x64" }
+		linkoptions {
+			"/ignore:4199", -- LNK4199: /DELAYLOAD:*.dll ignored; no imports found from *.dll
+		}
+		links { -- this is needed only for testing with GLES2/3 on Windows with VS2008
+			"DelayImp",
+		}
+
+	configuration { "vs201*", "x32 or x64" }
+		linkoptions { -- this is needed only for testing with GLES2/3 on Windows with VS201x
+			"/DELAYLOAD:\"libEGL.dll\"",
+			"/DELAYLOAD:\"libGLESv2.dll\"",
+		}
+
+	configuration { "mingw*" }
+		targetextension ".exe"
+		links {
+			"gdi32",
+			"psapi",
+		}
+
+	configuration { "vs20*", "x32 or x64" }
+		links {
+			"gdi32",
+			"psapi",
+		}
+
+	configuration { "mingw-clang" }
+		kind "ConsoleApp"
+
+	configuration { "android*" }
+		kind "ConsoleApp"
+		targetextension ".so"
+		linkoptions {
+			"-shared",
+		}
+		links {
+			"EGL",
+			"GLESv2",
+		}
+
+	configuration { "nacl*" }
+		kind "ConsoleApp"
+		targetextension ".nexe"
+		links {
+			"ppapi",
+			"ppapi_gles2",
+			"pthread",
+		}
+
+	configuration { "pnacl" }
+		kind "ConsoleApp"
+		targetextension ".pexe"
+		links {
+			"ppapi",
+			"ppapi_gles2",
+			"pthread",
+		}
+
+	configuration { "asmjs" }
+		kind "ConsoleApp"
+		targetextension ".bc"
+
+	configuration { "linux-* or freebsd", "not linux-steamlink" }
+		links {
+			"X11",
+			"GL",
+			"pthread",
+		}
+
+	configuration { "linux-steamlink" }
+		links {
+			"EGL",
+			"GLESv2",
+			"SDL2",
+			"pthread",
+		}
+
+	configuration { "rpi" }
+		links {
+			"X11",
+			"GLESv2",
+			"EGL",
+			"bcm_host",
+			"vcos",
+			"vchiq_arm",
+			"pthread",
+		}
+
+	configuration { "osx" }
+		linkoptions {
+			"-framework Cocoa",
+			"-framework QuartzCore",
+			"-framework OpenGL",
+			"-weak_framework Metal",
+		}
+
+	configuration {}
+
+	strip()
+end
+
+dofile (path.join(BGFX_DIR, "scripts", "bgfx.lua"))
+
+group "common"
+dofile (path.join(BGFX_DIR, "scripts", "example-common.lua"))
+
+group "libs"
+bgfxProject("", "StaticLib", {})
+
+group "main"
+retroCommander()
+
---
+++ src/main.cpp
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2011-2016 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
+ */
+
+#include <bx/uint32_t.h>
+#include "common.h"
+#include "bgfx_utils.h"
+#include "entry/cmd.h"
+#include "ocornut-imgui/imgui.h"
+
+
+inline void SetupImGuiStyle(bool bStyleDark_, float alpha_)
+{
+	ImGuiStyle& style = ImGui::GetStyle();
+
+	// light style from Pacôme Danhiez (user itamago) https://github.com/ocornut/imgui/pull/511#issuecomment-175719267
+	style.Alpha = 1.0f;
+	style.FrameRounding = 3.0f;
+	style.Colors[ImGuiCol_Text] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
+	style.Colors[ImGuiCol_WindowBg] = ImVec4(0.94f, 0.94f, 0.94f, 0.94f);
+	style.Colors[ImGuiCol_ChildWindowBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	style.Colors[ImGuiCol_PopupBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.94f);
+	style.Colors[ImGuiCol_Border] = ImVec4(0.00f, 0.00f, 0.00f, 0.39f);
+	style.Colors[ImGuiCol_BorderShadow] = ImVec4(1.00f, 1.00f, 1.00f, 0.10f);
+	style.Colors[ImGuiCol_FrameBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.94f);
+	style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
+	style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
+	style.Colors[ImGuiCol_TitleBg] = ImVec4(0.96f, 0.96f, 0.96f, 1.00f);
+	style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(1.00f, 1.00f, 1.00f, 0.51f);
+	style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.82f, 0.82f, 0.82f, 1.00f);
+	style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
+	style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.98f, 0.98f, 0.98f, 0.53f);
+	style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.69f, 0.69f, 0.69f, 1.00f);
+	style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.59f, 0.59f, 0.59f, 1.00f);
+	style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.49f, 0.49f, 0.49f, 1.00f);
+	style.Colors[ImGuiCol_ComboBg] = ImVec4(0.86f, 0.86f, 0.86f, 0.99f);
+	style.Colors[ImGuiCol_CheckMark] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.24f, 0.52f, 0.88f, 1.00f);
+	style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_Button] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
+	style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.06f, 0.53f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_Header] = ImVec4(0.26f, 0.59f, 0.98f, 0.31f);
+	style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
+	style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_Column] = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+	style.Colors[ImGuiCol_ColumnHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.78f);
+	style.Colors[ImGuiCol_ColumnActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_ResizeGrip] = ImVec4(1.00f, 1.00f, 1.00f, 0.50f);
+	style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
+	style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
+	style.Colors[ImGuiCol_CloseButton] = ImVec4(0.59f, 0.59f, 0.59f, 0.50f);
+	style.Colors[ImGuiCol_CloseButtonHovered] = ImVec4(0.98f, 0.39f, 0.36f, 1.00f);
+	style.Colors[ImGuiCol_CloseButtonActive] = ImVec4(0.98f, 0.39f, 0.36f, 1.00f);
+	style.Colors[ImGuiCol_PlotLines] = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+	style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+	style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+	style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+	style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
+	style.Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.20f, 0.20f, 0.20f, 0.35f);
+
+	if (bStyleDark_)
+	{
+		for (int i = 0; i <= ImGuiCol_COUNT; i++)
+		{
+			ImVec4& col = style.Colors[i];
+			float H, S, V;
+			ImGui::ColorConvertRGBtoHSV(col.x, col.y, col.z, H, S, V);
+
+			if (S < 0.1f)
+			{
+				V = 1.0f - V;
+			}
+			ImGui::ColorConvertHSVtoRGB(H, S, V, col.x, col.y, col.z);
+			if (col.w < 1.00f)
+			{
+				col.w *= alpha_;
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i <= ImGuiCol_COUNT; i++)
+		{
+			ImVec4& col = style.Colors[i];
+			if (col.w < 1.00f)
+			{
+				col.x *= alpha_;
+				col.y *= alpha_;
+				col.z *= alpha_;
+				col.w *= alpha_;
+			}
+		}
+	}
+}
+
+class mainapp : public entry::AppI
+{
+public:
+	void init(int _argc, char** _argv) override;
+	virtual int shutdown() override;
+	bool update() override;
+
+	uint32_t m_width;
+	uint32_t m_height;
+	uint32_t m_debug;
+	uint32_t m_reset;
+};
+
+ENTRY_IMPLEMENT_MAIN(mainapp);
+
+void mainapp::init(int _argc, char** _argv)
+{
+	Args args(_argc, _argv);
+
+	m_width  = 1280;
+	m_height = 720;
+	m_debug  = BGFX_DEBUG_NONE;
+	m_reset  = BGFX_RESET_NONE;
+
+	// Disable commands from BGFX
+	cmdShutdown();
+	cmdInit();
+
+	bgfx::init(args.m_type, args.m_pciId);
+	bgfx::reset(m_width, m_height, m_reset);
+
+	// Enable debug text.
+	bgfx::setDebug(m_debug);
+
+	// Set view 0 clear state.
+	bgfx::setViewClear(0
+			, BGFX_CLEAR_COLOR|BGFX_CLEAR_DEPTH
+			, 0x000000ff
+			, 1.0f
+			, 0
+			);
+}
+
+int mainapp::shutdown()
+{
+	// Shutdown bgfx.
+	bgfx::shutdown();
+
+	return 0;
+}
+
+bool mainapp::update()
+{
+	if (!entry::processEvents(m_width, m_height, m_debug, m_reset) )
+	{
+		bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height) );
+		bgfx::touch(0);
+
+		bgfx::frame();
+		return true;
+	}
+
+	return false;
+}
